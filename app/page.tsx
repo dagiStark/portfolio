1ff8625b9f1d632e15e96e0123b1e15e79119af3
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-07-15 17:13:48 +0300

    edited about page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 import About from "@/components/About";
 import Footer from "@/components/Footer";
+import { FloatingNav } from "@/components/ui/FloatingNavBar";
 import Image from "next/image";
 
 export default function Home() {
+
+  const navItems = [
+    {
+      name: "About",
+      link: "/"
+    }
+  ]
+
   return (
     <main className="relative bg-black-100 flex flex-col justify-center items-center mx-auto sm:px-10 px-5">
+      <FloatingNav navItems={navItems}/>
       <About />
       <Footer />
     </main>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
+    <div className='text-4xl relative'>
       footer
     </div>
   )
